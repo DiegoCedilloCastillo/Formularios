446bb49ab23c299f7263d0f8633f39e1e2829376
Diego Cedillo <92384848+DiegoCedilloCastillo@users.noreply.github.com>
2021-11-04 06:54:05 -0600

    Add files via upload

---
+++ Formularios Intento 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+namespace readwriteapp
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter("C:/Users/dcedi/source/repos/Formularios Intento 2/Texto.txt");
+                sw.WriteLine("DOOM fue lanzado en 1993.");
+                sw.WriteLine("Es un juego de disparos en primera persona.");
+                sw.WriteLine("Cuenta con cuatro capítulos de nueve niveles cada uno.");
+                sw.WriteLine("El Doom original pesaba 11MB, comprimido en 4 disquetes.");
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finalizando.");
+            }
+        }
+    }
+}
